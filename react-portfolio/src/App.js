b780e6ca936e450da90c1d21cfeb1159b043cf04
Liam Rumgay <Liamrumgay2@gmail.com>
2021-10-17 22:04:16 -0500

    projects added, more features to be added

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
+import Project from "./components/Project"
 
 
 export default function app () {
   return (
     <main>
       <Navbar />
+      <Project />
       <Footer />
     </main>
   );
---
+++ react-portfolio/src/data.js
@@ -0,0 +1,17 @@
+export const projects = [
+    {
+        title:"The Martian Market",
+        description:"The Martian Market is a small simulation/mockup of what a market place for Mars would look like.",
+        image:"./MartianMarket.png",
+    },
+    {
+        title:"EmployeeTracker",
+        description:"Employee Tracker, is a small application designed to show the use of SQL, by building out a database to show all employees their roles, wage, and what department they work for.",
+        image:"./Employee-tracker.png"
+    },
+    {
+        title:"Wake Me Up!",
+        description:"Wake Me Up is a very simple alarm that does not let the user choose their desired ringtone, instead, you will be awoken to Black Metal at full volume.",
+        image:"./wakeMeUp.jpg",
+    },
+];
